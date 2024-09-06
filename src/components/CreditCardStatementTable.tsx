d91d11a7e99905cd1780567294a9754ff92666d9
Ted Shaffer <shaffer.family@gmail.com>
2024-09-06 08:12:41 -0700

    Next Statement / Previous Statement

--- src/components/CreditCardStatementTable.tsx
+++ src/components/CreditCardStatementTable.tsx
@@ -1,24 +1,20 @@
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useTypedSelector } from '../types';
+import { useNavigate, useParams } from 'react-router-dom';
+import { CreditCardStatement, useDispatch, useTypedSelector } from '../types';
 
 import { CreditCardTransactionRowInStatementTableProperties } from '../types';
-import { isNil } from 'lodash';
-import { getCreditCardTransactionRowInStatementTableProperties } from '../selectors';
+import { getCreditCardStatements, getCreditCardTransactionRowInStatementTableProperties } from '../selectors';
 import OverrideTransactionCategoriesDialog from './OverrideTransactionCategoriesDialog';
 import CreditCardStatementTransactionRow from './CreditCardStatementTransactionRow';
-import { updateCategoryInTransactions } from '../controllers';
+import { loadTransactions, updateCategoryInTransactions } from '../controllers';
+import { cloneDeep } from 'lodash';
 
-const CreditCardStatementTable: React.FC = () => {
+const CreditCardStatementTable: React.FC = (): any => {
 
   const { id } = useParams<{ id: string }>();
-
-  if (isNil(id)) {
-    return null;
-  }
-
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [sortColumn, setSortColumn] = useState<string>('transactionDate');
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
@@ -26,6 +22,31 @@ const CreditCardStatementTable: React.FC = () => {
   const [showOverrideTransactionCategoriesDialog, setShowOverrideTransactionCategoriesDialog] = React.useState(false);
 
   const creditCardTransactionRows: CreditCardTransactionRowInStatementTableProperties[] = useTypedSelector(state => getCreditCardTransactionRowInStatementTableProperties(state, id!));
+
+  const statements: CreditCardStatement[] = useTypedSelector(state => getCreditCardStatements(state));
+  let sortedStatements = cloneDeep(statements);
+  sortedStatements = sortedStatements.sort((a, b) => b.endDate.localeCompare(a.endDate));
+
+  function findStatementIndexById(statements: CreditCardStatement[], id: string): number {
+    return statements.findIndex(statement => statement.id === id);
+  }
+
+  const indexOfId: number = findStatementIndexById(sortedStatements, id!);
+  if (indexOfId === -1) {
+    throw new Error(`Statement with id ${id} not found`);
+  }
+
+  let nextStatement: CreditCardStatement | undefined = undefined;
+  let previousStatement: CreditCardStatement | undefined = undefined;
+
+  if (indexOfId === 0) {
+    nextStatement = sortedStatements[indexOfId + 1];
+  } else if (indexOfId === sortedStatements.length - 1) {
+    previousStatement = sortedStatements[indexOfId - 1];
+  } else {
+    previousStatement = sortedStatements[indexOfId - 1];
+    nextStatement = sortedStatements[indexOfId + 1];
+  }
 
   const handleSort = (column: string) => {
     if (sortColumn === column) {
@@ -70,10 +91,10 @@ const CreditCardStatementTable: React.FC = () => {
     const currentIndex = sortedTransactions.findIndex(transaction => transaction.id === transactionId);
     const isShiftPressed = (event.nativeEvent as MouseEvent).shiftKey;
     const newSelectedTransactionIds = new Set(selectedTransactionIds);
-  
+
     if (isShiftPressed && lastSelectedIndexRef.current !== null) {
       const [start, end] = [Math.min(currentIndex, lastSelectedIndexRef.current), Math.max(currentIndex, lastSelectedIndexRef.current)];
-      
+
       for (let i = start; i <= end; i++) {
         if (checked) {
           newSelectedTransactionIds.add(sortedTransactions[i].id);
@@ -89,13 +110,20 @@ const CreditCardStatementTable: React.FC = () => {
       }
       lastSelectedIndexRef.current = currentIndex;
     }
-  
+
     setSelectedTransactionId(newSelectedTransactionIds);
   }
 
   const renderSortIndicator = (column: string) => {
     if (sortColumn !== column) return null;
     return sortOrder === 'asc' ? ' ▲' : ' ▼';
+  };
+
+  const navigateToStatement = (creditCardStatement: CreditCardStatement) => {
+    dispatch(loadTransactions(creditCardStatement.startDate, creditCardStatement.endDate, true, false))
+      .then(() => {
+        navigate(`/statements/credit-card/${creditCardStatement.id}`);
+      });
   };
 
   return (
@@ -105,7 +133,21 @@ const CreditCardStatementTable: React.FC = () => {
         onClose={handleCloseOverrideTransactionCategoriesDialog}
         onSave={handleSaveOverrideTransactionCategories}
       />
-      <Button
+      <Box display="flex" justifyContent="space-between" mb={2} >
+        <Button
+          onClick={() => navigateToStatement(previousStatement!)}
+          disabled={!previousStatement}
+        >
+          Previous Statement
+        </Button>
+        < Button
+          onClick={() => navigateToStatement(nextStatement!)}
+          disabled={!nextStatement}
+        >
+          Next Statement
+        </Button>
+      </Box>
+      < Button
         onClick={() => handleOverrideTransactionCategories()}
         disabled={selectedTransactionIds.size === 0}
       >
