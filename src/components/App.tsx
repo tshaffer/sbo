52e22743d16790bdfe2a47e4409db486152251f2
Ted Shaffer <shaffer.family@gmail.com>
2024-09-07 05:19:32 -0700

    step 1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,14 +8,16 @@ import Layout from './Layout';
 import Statements from './Statements';
 import CheckingAccountStatementsTable from './StatementsTable';
 import CreditCardStatementsTable from './StatementsTable';
-// import CreditCardStatementTable from './CreditCardStatementTable';
 import CategoryAssignmentRulesTable from './CategoryAssignmentRulesTable';
 import ReportsContent from './ReportsContent';
-// import CheckingAccountStatementTable from './CheckingAccountStatementTable';
 import CategoriesContent from './CategoriesContent';
-import { TrackerVoidPromiseThunkAction } from '../types';
 import CreditCardStatementTable from './CreditCardStatementTable';
 import CheckingAccountStatementTable from './CheckingAccountStatementTable';
+import { loadTransactions } from '../controllers/transactions';
+
+const handleLoadTransactions = (startDate: string, endDate: string, includeCreditCardTransactions: boolean, includeCheckingAccountTransactions: boolean): any => {
+  loadTransactions(startDate, endDate, includeCreditCardTransactions, includeCheckingAccountTransactions);
+}
 
 const router = createBrowserRouter([
   {
@@ -28,9 +30,7 @@ const router = createBrowserRouter([
         children: [
           {
             path: 'credit-card',
-            element: <CreditCardStatementsTable statements={[]} onLoadTransactions={function (startDate: string, endDate: string): TrackerVoidPromiseThunkAction {
-              throw new Error('Function not implemented.');
-            } } gridTemplateColumns={''} />,
+            element: <CreditCardStatementsTable statements={[]} onLoadTransactions={(startDate, endDate) => handleLoadTransactions(startDate, endDate, true, false)} gridTemplateColumns={'40px 1fr 1fr 1fr 1fr 1fr'} />,
           },
           {
             path: 'credit-card/:id',
@@ -38,9 +38,7 @@ const router = createBrowserRouter([
           },
           {
             path: 'checking-account',
-            element: <CheckingAccountStatementsTable statements={[]} onLoadTransactions={function (startDate: string, endDate: string): TrackerVoidPromiseThunkAction {
-              throw new Error('Function not implemented.');
-            } } gridTemplateColumns={''} />,
+            element: <CheckingAccountStatementsTable statements={[]} onLoadTransactions={(startDate, endDate) => handleLoadTransactions(startDate, endDate, false, true)} gridTemplateColumns={'40px 1fr 1fr 1fr 1fr 1fr 1fr 1fr'} />,
           },
           {
             path: 'checking-account/:id',
