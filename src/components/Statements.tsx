a167993ba87e951ad1bb7039bc03d12e4242922d
Ted Shaffer <shaffer.family@gmail.com>
2024-09-11 05:29:01 -0700

    Restore Upload button to SBO.

--- src/components/Statements.tsx
+++ src/components/Statements.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from 'react';
-import { Tabs, Tab, Box, Typography } from '@mui/material';
+
 import { Outlet, useNavigate, useLocation } from 'react-router-dom';
+
+import { Tabs, Tab, Box, Typography, Button } from '@mui/material';
+import UploadIcon from '@mui/icons-material/Upload';
+
 import { SidebarMenuButton } from '../types';
+import UploadStatementDialog from './UploadStatementDialog';
 
 const Statements: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const [showUploadStatementDialog, setShowUploadStatementDialog] = React.useState(false);
+
   const selectedTab = location.pathname.includes('checking-account') ? 'checking-account' : 'credit-card';
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: 'credit-card' | 'checking-account') => {
     navigate(`/statements/${newValue}`);
+  };
+
+  const handleCloseUploadStatementDialog = () => {
+    setShowUploadStatementDialog(false);
   };
 
   useEffect(() => {
@@ -20,14 +31,28 @@ const Statements: React.FC = () => {
   }, [location, navigate]);
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Typography variant="h5">{SidebarMenuButton.Statements}</Typography>
-      <Tabs value={selectedTab} onChange={handleTabChange}>
-        <Tab label="Credit Card" value="credit-card" />
-        <Tab label="Checking Account" value="checking-account" />
-      </Tabs>
-      <Outlet />
-    </Box>
+    <React.Fragment>
+      <UploadStatementDialog
+        open={showUploadStatementDialog}
+        onClose={handleCloseUploadStatementDialog}
+      />
+      <Box sx={{ width: '100%' }}>
+        <Typography variant="h5">{SidebarMenuButton.Statements}</Typography>
+        <Tabs value={selectedTab} onChange={handleTabChange}>
+          <Tab label="Credit Card" value="credit-card" />
+          <Tab label="Checking Account" value="checking-account" />
+        </Tabs>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+          <Button
+            startIcon={<UploadIcon />}
+            onClick={() => setShowUploadStatementDialog(true)}
+          >
+            Upload
+          </Button>
+        </Box>
+        <Outlet />
+      </Box>
+    </React.Fragment>
   );
 };
 
