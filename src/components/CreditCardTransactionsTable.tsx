40ead8625602047fa78a5e9aa10dff760784f85c
Ted Shaffer <shaffer.family@gmail.com>
2024-09-13 07:54:59 -0700

    Restore OverrideTransactions functionality, though not fully tested.

--- src/components/CreditCardTransactionsTable.tsx
+++ src/components/CreditCardTransactionsTable.tsx
@@ -6,39 +6,59 @@ import { CreditCardStatement, CreditCardTransactionRowInStatementTableProperties
 import { loadTransactions, updateCategoryInTransactions } from '../controllers';
 import CreditCardStatementTransactionRow from './CreditCardStatementTransactionRow';
 import TransactionsTable from './TransactionsTable';
+import OverrideTransactionCategoriesDialog from './OverrideTransactionCategoriesDialog';
 
 const CreditCardTransactionsTable: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const dispatch = useDispatch();
 
+  const [selectedTransactionIds, setSelectedTransactionId] = React.useState<Set<string>>(new Set());
+  const [showOverrideTransactionCategoriesDialog, setShowOverrideTransactionCategoriesDialog] = React.useState(false);
+
   const statements: Statement[] = useTypedSelector(getCreditCardStatements);
   const transactions: CreditCardTransactionRowInStatementTableProperties[] = useTypedSelector(state => getCreditCardTransactionRowInStatementTableProperties(state, id!));
 
   const handleOverrideTransactionCategories = (selectedTransactionIds: Set<string>) => {
+    setSelectedTransactionId(selectedTransactionIds);
+    setShowOverrideTransactionCategoriesDialog(true);
+  };
+
+  const handleSaveOverrideTransactionCategories = (categoryId: string) => {
     dispatch(updateCategoryInTransactions(
-      'someCategoryId', // Replace with the actual category ID
-      Array.from(selectedTransactionIds)
+      categoryId,
+      Array.from(selectedTransactionIds),
     ));
   };
 
+  const handleCloseOverrideTransactionCategoriesDialog = () => {
+    setShowOverrideTransactionCategoriesDialog(false);
+  }
+
   return (
-    <TransactionsTable
-      statements={statements}
-      transactions={transactions}
-      onOverrideTransactionCategories={handleOverrideTransactionCategories}
-      getTransactionId={(transaction: CreditCardTransactionRowInStatementTableProperties) => transaction.id}
-      getStatementId={(statement: CreditCardStatement) => `credit-card/${statement.id}`}
-      renderTransactionRow={(transaction, selectedTransactionIds, handleTransactionSelectedChanged) => (
-        <CreditCardStatementTransactionRow
-          creditCardTransactionId={transaction.id}
-          transactionSelected={selectedTransactionIds.has(transaction.id)}
-          onTransactionSelectedChanged={handleTransactionSelectedChanged}
-        />
-      )}
-      columnHeaders={['', 'Date', 'Amount', '', '', 'Description', 'Category', 'Comment', '']}
-      columnKeys={['', 'transactionDate', 'amount', '', '', 'userDescription', 'categorizedTransactionName', 'comment', '']}
-      tableContainerClassName="credit-card-statement-grid-table-container"
-    />
+    <React.Fragment>
+      <OverrideTransactionCategoriesDialog
+        open={showOverrideTransactionCategoriesDialog}
+        onClose={handleCloseOverrideTransactionCategoriesDialog}
+        onSave={handleSaveOverrideTransactionCategories}
+      />
+      <TransactionsTable
+        statements={statements}
+        transactions={transactions}
+        onOverrideTransactionCategories={handleOverrideTransactionCategories}
+        getTransactionId={(transaction: CreditCardTransactionRowInStatementTableProperties) => transaction.id}
+        getStatementId={(statement: CreditCardStatement) => `credit-card/${statement.id}`}
+        renderTransactionRow={(transaction, selectedTransactionIds, handleTransactionSelectedChanged) => (
+          <CreditCardStatementTransactionRow
+            creditCardTransactionId={transaction.id}
+            transactionSelected={selectedTransactionIds.has(transaction.id)}
+            onTransactionSelectedChanged={handleTransactionSelectedChanged}
+          />
+        )}
+        columnHeaders={['', 'Date', 'Amount', '', '', 'Description', 'Category', 'Comment', '']}
+        columnKeys={['', 'transactionDate', 'amount', '', '', 'userDescription', 'categorizedTransactionName', 'comment', '']}
+        tableContainerClassName="credit-card-statement-grid-table-container"
+      />
+    </React.Fragment>
   );
 };
 
